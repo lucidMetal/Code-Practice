5faeee660259c6a1d8bd3e57834d409fad69b0b4
solevicinity <michaelt.abayneh@gmail.com>
2021-10-03 18:20:36 -0400

    JS Session - Functions, Data Types, Lists

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Strings, Numbers, Booleans
+
+/* Type Coercion
+if (condition) {
+    actual thing to be done
+} else if (condition) {
+    actual thing to be done
+} else {
+    actual things to be done
+}
+*/
+
+/*
+If sunny, display "Happy"
+If cloudy, display "Sad"
+If neither, display, "Happy && Sad"
+*/
+// const weather = "rainy";
+
+// if (weather === "sunny") {
+//   console.log("happy");
+// } else if (weather === "cloudy") {
+//   console.log("sad");
+// } else {
+//   console.log("Happy && Sad");
+// }
+
+// hello("Etsub", 17, "male");
+
+// function hello(name, age, gender) {
+//   console.log(`Hello, my name is ${name} and I am ${age}. I am ${gender}.`);
+// }
+
+// const helloPerson = (name, age, gender) => {
+//   console.log(`Hello, my name is ${name} and I am ${age}. I am ${gender}.`);
+// };
+
+// helloPerson("Michael", 15, "male");
+
+const numbersList = [1, 3, "Hello", true];
